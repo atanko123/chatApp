d59d769e55f9cd0af00903c683bf5c1072096656
atanko123 <anze.8tanko@gmail.com>
2020-12-28 22:27:55 +0100

    Added date to msgs

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,12 +3,24 @@ import React from "react"
 // Images
 import avatar from "../images/avatar.png"
 
+function parseDate(time) {
+
+    if (time) {
+        const d = time.toDate().toString().split(" ")
+        const t = d[4].split(":")
+        return `${d[2]}/${d[1]}/${d[3]} ${t[0]}:${t[1]}`
+    }
+
+    return null
+}
+
 function Message(props) {
     const {msg, isMe, time, photoURL} = props
     let profilePic = avatar
     if (photoURL) {
         profilePic = photoURL
     }
+    const date = parseDate(time)
 
     return (
 
@@ -19,7 +31,7 @@ function Message(props) {
             <div className={ `msg-text ${isMe ? "my-text" : "your-text"}` }>
                 {msg}
                 <div className="msg-date">
-                    25/23/2020 17:12
+                    { date }
                 </div>
             </div>
         </div>
