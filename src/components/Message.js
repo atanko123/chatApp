68d80c0cd2d1798cf91afa46db362efbb82d7502
atanko123 <anze.8tanko@gmail.com>
2020-12-29 23:03:01 +0100

    Fixed bug with footer and show name on hover!

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,42 +1,80 @@
-import React from "react"
+import React, { Component } from "react"
 
 // Images
 import avatar from "../images/avatar.png"
 
-function parseDate(time) {
+class Message extends Component {
 
-    if (time) {
-        const d = time.toDate().toString().split(" ")
-        const t = d[4].split(":")
-        return `${d[2]}/${d[1]}/${d[3]} ${t[0]}:${t[1]}`
+    constructor() {
+        super()
+        this.state = {
+            showName: false
+        }
     }
 
-    return null
-}
-
-function Message(props) {
-    const {msg, isMe, time, photoURL} = props
-    let profilePic = avatar
-    if (photoURL) {
-        profilePic = photoURL
+    parseDate(time) {
+        if (time) {
+            const d = time.toDate().toString().split(" ")
+            const t = d[4].split(":")
+            return `${d[2]}/${d[1]}/${d[3]} ${t[0]}:${t[1]}`
+        }
+    
+        return null
     }
-    const date = parseDate(time)
 
+    mouseOverHandler(e) {
+        e.preventDefault()
+        if (!this.state.showName) {
+            this.setState({
+                showName: true
+            })
+        }
+    }
 
-    return (
+    mouseLeaveHandler(e) {
+        e.preventDefault()
+        if (this.state.showName) {
+            this.setState({
+                showName: false
+            })
+        }
+    }
 
-        <div className={ isMe ? "my-text-container" : "your-text-container" }>
-            <div className={ `${isMe ? "pic-right" : "pic-left"}`}>
-                <img className="msg-pic" src={profilePic} alt=":)" />
-            </div>
-            <div className={ `msg-text ${isMe ? "my-text" : "your-text"}` }>
-                {msg}
-                <div className="msg-date">
-                    { date }
+    render() {
+        const {
+            msg,
+            isMe,
+            time,
+            photoURL,
+            last,
+            name
+        } = this.props
+
+        const profilePic = (() => {
+            return photoURL ? photoURL : avatar
+        })()
+        const date = this.parseDate(time)
+
+        return (
+
+            <div className={ `${last} ${isMe ? "my-text-container" : "your-text-container"}` }>
+                <div className={ `${isMe ? "pic-right" : "pic-left"}`}>
+                    <img className="msg-pic" src={profilePic} alt=":)" />
                 </div>
+                <div onMouseOver={(e) => this.mouseOverHandler(e)}
+                    onMouseLeave={(e) => this.mouseLeaveHandler(e)}
+                    className={ `msg-text ${isMe ? "my-text" : "your-text"}` }>
+                    {msg}
+                    <div className="msg-date">
+                        {date}
+                    </div>
+                </div>
+                { this.state.showName && 
+                    <div className={ `${isMe ? "my-name" : "your-name"}`}>{name}</div> 
+                }
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Message
